abd1b0a05a5ebc47688a396ee3aa6c5b0640313a
Davis Hunt <vdh3@mac.com>
2016-03-23 10:38:24 -0500

    fields added to user.

--- neon/user/submodels/user.py
+++ neon/user/submodels/user.py
@@ -64,9 +64,14 @@ class NeonUser(AbstractBaseUser):
 	name = models.CharField(max_length=30,blank=True)
 	username = models.CharField(_('username'), max_length=30, blank=True)
 	email = models.EmailField(_('email address'), unique=True)
+
+	pic = models.ImageField(upload_to='user/', blank=True, default='')
+	bio = models.CharField(_('bio'), max_length=200, blank=True, default='')
+
 	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
 	phone = models.CharField(unique=True,validators=[phone_regex], blank=True, null=True,max_length=200) # validators should be a list
 	phone_verified = models.BooleanField(default=False,blank=False)
+
 	pin_hash  = models.TextField(max_length=200,blank=True)
 	pin_sent =  models.BooleanField(default=False,blank=False)
 	
