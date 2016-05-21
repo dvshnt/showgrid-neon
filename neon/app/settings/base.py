c20bf41cce9a3b69b926c31786bb0f5e619ba085
Davis Hunt <vdh3@mac.com>
2016-05-21 09:03:15 -0500

    Remove deprecated Template loaders

--- neon/app/settings/base.py
+++ neon/app/settings/base.py
@@ -78,15 +78,6 @@ STATICFILES_FINDERS = (
 )
 
 
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
-
-
 MIDDLEWARE_CLASSES = (
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -108,12 +99,6 @@ ROOT_URLCONF = 'app.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'app.wsgi.application'
-
-
-from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
-TEMPLATE_CONTEXT_PROCESSORS = TCP + (
-    'django.core.context_processors.request',
-)
 
 
 INSTALLED_APPS = (
